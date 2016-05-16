ce35c4bb37352c5894bc7e1270bc39b080e44e1d
HaiboZhu <hbzhu@thoughtworks.com>
2016-05-16 17:44:16 +0800

    Before ListView

--- index.ios.js
+++ index.ios.js
@@ -6,47 +6,91 @@
 
 import React, { Component } from 'react';
 import {
+  ListView,
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+var MOCKED_MOVIES_DATA = [
+  {title:'Title',year:'2015',posters:{thumbnail:'http://i.imgur.com/UePbdph.jpg'}},
+];
+
 class AwesomeProject extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+     movies: null,
+    };
+  }
+  componentDidMount() {
+   this.fetchData();
+  }
+  fetchData() {
+   fetch(REQUEST_URL) 
+     .then((response) => response.json())
+     .then((responseData) => {
+       this.setState({
+         movies: responseData.movies,
+       });
+     })
+     .done();
+  }
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+          Loading movies....
         </Text>
       </View>
-    );
+      );
   }
-}
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+      );
+  }
+  render() {
+    if(!this.state.movies) {
+      return this.renderLoadingView();
+    }
+    var movie =  this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  thumbnail: {
+    width: 53,
+    height: 81,
   },
-  instructions: {
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
